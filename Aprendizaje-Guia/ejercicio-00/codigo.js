7634033cae593ff550508c3600f75ff619a61164
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-12-12 13:34:12 -0500

    nuevo cmmit

--- Aprendizaje-Guia/ejercicio-00/codigo.js
+++
@@ -1,9 +0,0 @@
-function solicitarDatos(){
-    nombre = prompt('¿Cual es tu nombre?');
-    apellidos = prompt('¿Cuales son tus Apellidos?');
-
-    usuario = nombre.concat(apellidos);
-
-    //alert(`Mi nombre es: ${nombre.concat(apellidos)}`);
-    alert(`Mi nombre es: ${usuario}`);
-}
