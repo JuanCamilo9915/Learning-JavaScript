b9f192bd691d0bb6a10bf1bf708fe044b1d9c074
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-12-12 15:22:30 -0500

    se crea la rama mi-portafolio-web

---
+++ MiPortafolio/app/js/btnSiguiente.js
@@ -0,0 +1,17 @@
+let contadorSigteInterfaz = 0;
+
+function btnSigte(sigteInterfaz) {
+    switch (sigteInterfaz) {
+        case 1:
+            alert('Si funciono, jjeej, va hacia adelante '+sigteInterfaz);
+            document.getElementById('');
+            break;
+
+        case -1:
+            alert('Si funciono, jjeej, va hacia atras '+sigteInterfaz);
+            break;
+        default:
+            alert('No funciono, :(, Error');
+            break;
+    }
+}
