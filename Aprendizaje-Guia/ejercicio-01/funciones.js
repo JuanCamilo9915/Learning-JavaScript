87b30ab29e6a3dc51d704bd9986a2d3bd9b793f3
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2022-01-16 22:44:06 -0500

    se elimina el archivo index.html de la carpeta ejercicio-01, y se agregan los archivos variables.js y funciones.js

---
+++ Aprendizaje-Guia/ejercicio-01/funciones.js
@@ -0,0 +1,15 @@
+console.log('\n|-------------Funciones-------------|');
+
+//Funciones declarativas
+function miFunction(){
+    console.log('Función sin parámetros');
+}
+
+function miFunction(name){
+    console.log('Función con parámetros, nombre: ' + name);
+}
+
+//Funciones anónimas
+let functionAnonymous = function (){
+    console.log('Función Anónima');
+}
